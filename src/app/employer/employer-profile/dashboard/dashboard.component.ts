d71309e3a359aaeedf7f32ffd69e146b996a865b
Vishal Nagar <vishal19.nagar@tataaig.com>
2025-09-25 15:56:06 +0530

    My Commit -1

--- src/app/employer/employer-profile/dashboard/dashboard.component.ts
+++ src/app/employer/employer-profile/dashboard/dashboard.component.ts
@@ -5,12 +5,13 @@ import { FollowingComponent } from '../following/following.component';
 import { JobPreferencesComponent } from '../job-preferences/job-preferences.component';
 import { NotificationComponent } from '../notification/notification.component';
 import { ProfileComponent } from '../profile/profile.component';
+import { JobActivityComponent } from '../job-activity/job-activity.component';
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [AccountSettingComponent, ContributionsComponent, FollowingComponent, JobPreferencesComponent, NotificationComponent, ProfileComponent],
+  imports: [AccountSettingComponent, ContributionsComponent, FollowingComponent, JobPreferencesComponent, NotificationComponent, ProfileComponent, JobActivityComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
--- src/app/employer/employer-profile/job-activity/job-activity.component.ts
+++ src/app/employer/employer-profile/job-activity/job-activity.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import this
+
 
 @Component({
   selector: 'app-job-activity',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-activity.component.html',
   styleUrl: './job-activity.component.css'
 })
 export class JobActivityComponent {
-
+    value:Number = 0;
+    changenumber(value1:Number){
+        this.value = value1;
+    }
 }
